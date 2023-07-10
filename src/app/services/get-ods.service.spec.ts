0906aab534695da2eb781a7c042365a9d6b8dcba
Carlos Kremer <cbpontokremer@gmail.com>
2023-07-10 15:27:16 -0300

    fixed wird login screen size

--- src/app/services/get-ods.service.spec.ts
+++ src/app/services/get-ods.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GetOdsService } from './get-ods.service';
+import { sequenceEqual } from 'rxjs';
 
 describe('GetOdsService', () => {
   let service: GetOdsService;
